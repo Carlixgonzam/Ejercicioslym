2d8dd19185d96254a5af48c6cb9812890118d53a
Carla Gonzalez <carlafernanda006@gmail.com>
2025-02-13 18:58:20 -0500

    cambioooooos

--- proyecto0.py
+++ proyecto0.py
@@ -1,113 +1,137 @@
-#Aca determinamos todas las reglas gramaticales que se tienen que tener en cuenta
-#Se habilitan las reglas gramaticales para el lenguaje de programación de robots
-
-# Se definen las reglas gramaticales del lenguaje del robot
 REGLAS_GRAMATICA = {
-    "program": ["definitiones instructiones"],
-    "definitiones": ["variables | procedimientos"],
-    "variables": ["'|' variable_list '|'"],
-    "variable_lista": ["identificador ',' variable_lista", "identificador"],
+    "program": ["{definitiones} {instrucciones}"],
+    "definitiones": ["{variables} | {procedimientos}"],  # Variables y procedimientos como bloques
+    "variables": ["'|' {variable_lista} '|'"],  # bloque de var
+    "variable_lista": ["{id} ',' variable_lista", "{id}"],
     "procedimientos": ["procedimiento procedimientos", "ε"],
-    "procedimiento": ["'proc' identificador parameter_list '[' bloque ']'"],
-    "parameter_list": ["':' identificador param_list", "ε"],
-    "param_list": ["'and' identificador ':' identificador param_list", "ε"],
-    "bloque": ["instruccion bloque", "ε"],
-    "instructiones": ["instruccion instructiones", "ε"],
-    "instruccion": ["command", "assignment", "conditional", "loop", "procedure_call"],
-    "command": ["'move:' number 'inDir:' direction '.'", "'turn:' direction '.'", "'put:' identificador 'ofType:' object_type '.'", "'pick:' identificador 'ofType:' object_type '.'", "'goTo:' number 'with:' number '.'", "'nop.'"],
-    "assignment": ["identificador ':=' expression '.'"],
-    "expression": ["number", "identificador"],
-    "conditional": ["'if:' condition 'then:' '[' bloque ']' 'else:' '[' bloque ']'"],
-    "condition": ["'facing:' direction", "'canMove:' number 'inDir:' direction", "'canPut:' number 'ofType:' object_type", "'canPick:' number 'ofType:' object_type", "'not:' condition"],
-    "loop": ["'while:' condition 'do:' '[' bloque ']'"],
-    "procedure_call": ["identificador argument_list '.'"],
-    "argument_list": ["':' expression argument_list", "ε"],
+    "procedimiento": ["'proc' {id} param_lista '[' {bloque} ']'"],
+    #"parameter_list": ["':' {id} param_lista", "ε"],
+    "param_lista": ["'and' | ':' {id} ':' {id} param_lista", "{bloque}"],
+    "bloque": ["instruccion {bloque}", "ε"],
+    "instrucciones": ["instruccion instrucciones", "ε"],
+    "instruccion": ["comando","asignar","condicional","ciclo","llamada_procedimiento",],
+    "comando": ["'move:' number 'inDir:' {direccion} '.'","'turn:' {direccion} '.'","'put:' id 'ofType:' object_type '.'","'pick:' identificador 'ofType:' object_type '.'","'goTo:' number 'with:' number '.'","'nop.'",],
+    "asignar": ["{id} ':=' expre '.'"],
+    "expre": ["number", "id"],
+    "condicional": ["'if:' {condicion} 'then:' '[' {bloque} ']' 'else:' '[' {bloque} ']'"],
+    "condicion": [
+        "'facing:' {direccion}",
+        "'canMove:' number 'inDir:' {direccion}",
+        "'canPut:' number 'ofType:' objeto_tipo",
+        "'canPick:' number 'ofType:' objeto_tipo",
+        "'not:' {condicion}",],
+    "ciclo": ["'while:' {condicion} 'do:' '[' {bloque} ']'"],
+    "llamada_procedimiento": ["id {argument_list} '.'"],
+    "argument_list": ["':' expre {argument_list}", "{bloque}"],
     "objeto_tipo": ["'#chips'", "'#balloons'"],
-    "direction": ["'#north'", "'#south'", "'#west'", "'#east'"],
+    "direccion": ["'#north'", "'#south'", "'#west'", "'#east'"],
     "number": ["[0-9]+"],
-    "identificador": ["[a-zA-Z_][a-zA-Z0-9_]*"]
-}
+    "id": ["[a-zA-Z_][a-zA-Z0-9_]*"],}
 
-# Función para tokenizar el código del robot
-def obtener_tokens(codigo):
-    lista_tokens = []
+
+def obtener(codigo):
+    lista = []
     i = 0
     while i < len(codigo):
         char = codigo[i]
-        
-        if char.isspace():
-            i += 1
-        elif char.isalpha() or char == '_':
-            inicio = i
-            while i < len(codigo) and (codigo[i].isalnum() or codigo[i] == '_'):
-                i += 1
-            valor_token = codigo[inicio:i]
-            # Se consideran algunas palabras reservadas
-            tipo_token = 'PALABRA_RESERVADA' if valor_token in {'proc', 'if:', 'else:', 'while:', 'repeatTimes:'} else 'IDENTIFICADOR'
-            lista_tokens.append((valor_token, tipo_token))
-        elif char.isdigit():
-            inicio = i
-            while i < len(codigo) and codigo[i].isdigit():
-                i += 1
-            lista_tokens.append((codigo[inicio:i], 'NUMERO'))
-        elif char in {':', '.', '|', '(', ')', '[', ']'}:
-            lista_tokens.append((char, 'SIMBOLO'))
-            i += 1
-        elif char == '#':
-            inicio = i
-            i += 1
-            while i < len(codigo) and codigo[i].isalpha():
-                i += 1
-            lista_tokens.append((codigo[inicio:i], 'CONSTANTE'))
-        else:
-            print(f"Error: Carácter inesperado '{char}'")
-            return None
-    
-    return lista_tokens
 
-# Función principal para analizar (parsear) el programa
-def analizar_programa(lista_tokens):
-    arbol_sintactico = {"Programa": []}
-    while lista_tokens:
-        token, tipo = lista_tokens.pop(0)
-        if tipo == 'PALABRA_RESERVADA' and token == 'proc':
-            arbol_sintactico["Programa"].append(analizar_procedimiento(lista_tokens))
-        elif tipo == 'PALABRA_RESERVADA' and token in {'if:', 'while:', 'repeatTimes:'}:
-            arbol_sintactico["Programa"].append(analizar_condicional(lista_tokens, token))
+        if char == " " or char == "\n" or char == "\t":
+            i += 1
+        elif (
+            (char >= "a" and char <= "z")
+            or (char >= "A" and char <= "Z")
+            or char == "_"
+        ):
+            inicio = i
+            while i < len(codigo) and (
+                (codigo[i] >= "a" and codigo[i] <= "z")
+                or (codigo[i] >= "A" and codigo[i] <= "Z")
+                or (codigo[i] >= "0" and codigo[i] <= "9")
+                or codigo[i] == "_"
+            ):
+                i += 1
+            valor = codigo[inicio:i]
+            tipo = "PALABRA_RESERVADA"
+            if valor in {"proc", "if:", "else:", "while:", "repeatTimes:"}:
+                tipo = "PALABRA_RESERVADA"
+            else:
+                "IDENTIFICADOR"
+            lista.append((valor, tipo))
+        elif char >= "0" and char <= "9":
+            inicio = i
+            while i < len(codigo) and (codigo[i] >= "0" and codigo[i] <= "9"):
+                i += 1
+            lista.append((codigo[inicio:i], "NUMERO"))
+        elif char in {":", ".", "|", "(", ")", "[", "]"}:
+            lista.append((char, "SIMBOLO"))
+            i += 1
+        elif char == "#":
+            inicio = i
+            i += 1
+            while i < len(codigo) and (
+                (codigo[i] >= "a" and codigo[i] <= "z")
+                or (codigo[i] >= "A" and codigo[i] <= "Z")
+            ):
+                i += 1
+            lista.append((codigo[inicio:i], "CONSTANTE"))
         else:
-            arbol_sintactico["Programa"].append(analizar_instruccion(token, lista_tokens))
+            print("Erroooooor '{char}'")
+            return None
+
+    return lista
+
+
+# parse
+def analizar_programa(lista):
+    arbol_sintactico = {"Program": []}
+    while lista:
+        car, tipo = lista.pop(0)
+        if tipo == "PALABRA_RESERVADA" and car == "proc":
+            arbol_sintactico["Program"].append(analizar_procedimiento(lista))
+        elif tipo == "PALABRA_RESERVADA" and car in {"if:", "while:", "repeatTimes:"}:
+            arbol_sintactico["Program"].append(analizar_condicional(lista, car))
+        else:
+            arbol_sintactico["Program"].append(analizar_instruccion(car, lista))
     return arbol_sintactico, True
 
-# Función para analizar la definición de un procedimiento
-def analizar_procedimiento(lista_tokens):
-    nombre_proc = lista_tokens.pop(0)[0]
-    lista_tokens.pop(0)  # Se descarta el símbolo "[" que abre el cuerpo
+
+# analizar la definición de un procedimiento
+def analizar_procedimiento(lista):
+    nombre_proc = lista.pop(0)[0]
+    lista.pop(0)  # descarta el símbolo "[" que abre el cotigo
     cuerpo = []
-    while lista_tokens and lista_tokens[0][0] != "]":
-        cuerpo.append(analizar_instruccion(lista_tokens.pop(0)[0], lista_tokens))
-    lista_tokens.pop(0)  # Se descarta el símbolo "]" que cierra el cuerpo
+    while lista and lista[0][0] != "]":
+        cuerpo.append(analizar_instruccion(lista.pop(0)[0], lista))
+    lista.pop(0)  # descarta el símbolo "]" que cierra el cuerpo
     return {"Procedimiento": {"nombre": nombre_proc, "cuerpo": cuerpo}}
 
-# Función para analizar una instrucción simple
-def analizar_instruccion(token, lista_tokens):
-    nodo_instruccion = {"Instruccion": token, "Parametros": []}
-    while lista_tokens and lista_tokens[0][1] in {'NUMERO', 'CONSTANTE', 'IDENTIFICADOR'}:
-        nodo_instruccion["Parametros"].append(lista_tokens.pop(0)[0])
-    return nodo_instruccion
 
-# Función para analizar estructuras condicionales (if)
-def analizar_condicional(lista_tokens, tipo_condicional):
-    condicion = lista_tokens.pop(0)[0]
+# analizar una instrucción simple
+def analizar_instruccion(punto, lista):
+    nodoinstruccion = {"Instruccion": punto, "Parametros": []}
+    while lista and lista[0][1] in {"NUMERO", "CONSTANTE", "IDENTIFICADOR"}:
+        nodoinstruccion["Parametros"].append(lista.pop(0)[0])
+    return nodoinstruccion
+
+
+# analiz estructuras condicionales (if)
+def analizar_condicional(lista, tipo_condicional):
+    condicion = lista.pop(0)[0]
     bloques = []
-    if lista_tokens and lista_tokens[0][0] == 'then:':
-        lista_tokens.pop(0)  # Se descarta el 'then:'
-        bloques.append(analizar_instruccion(lista_tokens.pop(0)[0], lista_tokens))
-    if lista_tokens and lista_tokens[0][0] == 'else:':
-        lista_tokens.pop(0)  # Se descarta el 'else:'
-        bloques.append(analizar_instruccion(lista_tokens.pop(0)[0], lista_tokens))
-    return {"Condicional": {"tipo": tipo_condicional, "condicion": condicion, "bloques": bloques}}
+    while lista and lista[0][0] != "]":
+        bloques.append(analizar_instruccion(lista.pop(0)[0], lista))
+    if lista and lista[0][0] == "else:":
+        lista.pop(0)
+        bloques.append(analizar_instruccion(lista.pop(0)[0], lista))
+    return {
+        "Condicional": {
+            "tipo": tipo_condicional,
+            "condicion": condicion,
+            "bloques": bloques,
+        }
+    }
 
-# Ejemplo de código del robot para probar el analizador sintáctico
+
 codigo_robot = """
 |x y|
 proc goNorth [ while: canMove: 1 inDir: #north do: [ move: 1 inDir: #north .] ]
@@ -120,9 +144,9 @@ repeatTimes: for: 5 repeat: [ move: 1 . ]
 if: facing: #north then: [ move: 2 .] else: [ turn: #right . ]
 """
 
-# Ejecución del análisis sintáctico
-tokens = obtener_tokens(codigo_robot)
-if tokens:
-    arbol, valido = analizar_programa(tokens)
+
+t = obtener(codigo_robot)
+if t:
+    arbol, valido = analizar_programa(t)
     print("Árbol Sintáctico:", arbol)
     print("Pertenece al lenguaje:", valido)
